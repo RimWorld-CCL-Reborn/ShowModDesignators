f076a9e5fb323032f734df9e321ab4ec9780259e
erdelf <erdelf.productions@gmail.com>
2018-05-14 19:02:44 +0200

    initial commit

---
+++ Source/ShowModDesignators/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle(title: "ShowModDesignators")]
+[assembly: AssemblyDescription(description: "")]
+[assembly: AssemblyConfiguration(configuration: "")]
+[assembly: AssemblyCompany(company: "")]
+[assembly: AssemblyProduct(product: "ShowModDesignators")]
+[assembly: AssemblyCopyright(copyright: "Copyright ©  2018")]
+[assembly: AssemblyTrademark(trademark: "")]
+[assembly: AssemblyCulture(culture: "")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(visibility: false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid(guid: "902a2abf-fe7d-41a9-82ab-7e6d57a65789")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// indem Sie "*" wie unten gezeigt eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion(version: "1.0.0.0")]
+[assembly: AssemblyFileVersion(version: "1.0.0.0")]
---
+++ Source/ShowModDesignators/ShowModDesignators.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+
+namespace ShowModDesignators
+{
+    using JetBrains.Annotations;
+    using RimWorld;
+    using Verse;
+
+    [StaticConstructorOnStartup]
+    [UsedImplicitly]
+    public class ShowModDesignators
+    {
+        static ShowModDesignators()
+        {
+            foreach (ModContentPack mcp in LoadedModManager.RunningMods.Where(predicate: mcp => !mcp.IsCoreMod))
+            {
+                foreach (Def def in mcp.AllDefs)
+                {
+                    if (!def.description.NullOrEmpty())
+                        def.description += $"\n({mcp.Name})";
+
+                    if (BackstoryDatabase.allBackstories.TryGetValue(key: def.defName, value: out Backstory bs))
+                        bs.baseDesc += $"\n({mcp.Name})";
+                }
+            }
+        }
+    }
+}
