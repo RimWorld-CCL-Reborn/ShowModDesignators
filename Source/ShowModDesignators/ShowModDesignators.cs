37776449a37554408d5fb63a2cb4ff46c660b835
erdelf <erdelf.productions@gmail.com>
2018-05-14 20:36:37 +0200

    added implied defs

--- Source/ShowModDesignators/ShowModDesignators.cs
+++ Source/ShowModDesignators/ShowModDesignators.cs
@@ -13,22 +13,37 @@ namespace ShowModDesignators
         static ShowModDesignators()
         {
             foreach (ModContentPack mcp in LoadedModManager.RunningMods.Where(predicate: mcp => !mcp.IsCoreMod))
-            {
                 foreach (Def def in mcp.AllDefs)
                 {
-                    if (!def.description.NullOrEmpty())
-                        def.description += $"\n({mcp.Name})";
+                    string nameAdd = $"\n({mcp.Name})";
 
-                    if(def is TraitDef trd)
+                    if (!def.description.NullOrEmpty())
+                        def.description += nameAdd;
+
+                    switch (def)
                     {
-                        foreach (TraitDegreeData trdd in trd.degreeDatas)
-                            trdd.description += $"\n({mcp.Name})";
+                        case TraitDef trd:
+                            foreach (TraitDegreeData trdd in trd.degreeDatas)
+                                trdd.description += nameAdd;
+                            break;
+                        case ThingDef td:
+                            if (td.race != null)
+                            {
+                                td.race.meatDef.description += nameAdd;
+                                td.race.corpseDef.description += nameAdd;
+                                td.race.leatherDef.description += nameAdd;
+                            }
+
+                            /*if (td.recipeMaker != null)
+                                DefDatabase<RecipeDef>.GetNamed(defName: "Make_" + td.defName).description += nameAdd;
+                            if(td.IsDrug)
+                                DefDatabase<RecipeDef>.GetNamed(defName: "Administer_" + td.defName).description += nameAdd;*/
+                            break;
                     }
 
                     if (BackstoryDatabase.allBackstories.TryGetValue(key: def.defName, value: out Backstory bs))
-                        bs.baseDesc += $"\n({mcp.Name})";
+                        bs.baseDesc += nameAdd;
                 }
-            }
         }
     }
 }
