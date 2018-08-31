6daef005ae716478e10594df7a259909ce8120fd
erdelf <erdelf.productions@gmail.com>
2018-08-31 13:18:47 +0200

    updated for leather expectance

--- Source/ShowModDesignators/ShowModDesignators.cs
+++ Source/ShowModDesignators/ShowModDesignators.cs
@@ -2,6 +2,7 @@
 
 namespace ShowModDesignators
 {
+    using System;
     using JetBrains.Annotations;
     using RimWorld;
     using Verse;
@@ -15,34 +16,46 @@ namespace ShowModDesignators
             foreach (ModContentPack mcp in LoadedModManager.RunningMods.Where(predicate: mcp => !mcp.IsCoreMod))
                 foreach (Def def in mcp.AllDefs)
                 {
-                    string nameAdd = $"\n({mcp.Name})";
-
-                    if (!def.description.NullOrEmpty())
-                        def.description += nameAdd;
-
-                    switch (def)
+                    try
                     {
-                        case TraitDef trd:
-                            foreach (TraitDegreeData trdd in trd.degreeDatas)
-                                trdd.description += nameAdd;
-                            break;
-                        case ThingDef td:
-                            if (td.race != null)
-                            {
-                                td.race.meatDef.description += nameAdd;
-                                td.race.corpseDef.description += nameAdd;
-                                td.race.leatherDef.description += nameAdd;
-                            }
+                        string nameAdd = $"\n({mcp.Name})";
 
-                            /*if (td.recipeMaker != null)
-                                DefDatabase<RecipeDef>.GetNamed(defName: "Make_" + td.defName).description += nameAdd;
-                            if(td.IsDrug)
-                                DefDatabase<RecipeDef>.GetNamed(defName: "Administer_" + td.defName).description += nameAdd;*/
-                            break;
+                        if (!def.description.NullOrEmpty())
+                            def.description += nameAdd;
+
+
+                        switch (def)
+                        {
+                            case TraitDef trd:
+                                foreach (TraitDegreeData trdd in trd.degreeDatas)
+                                    trdd.description += nameAdd;
+                                break;
+                            case ThingDef td:
+                                if (td.race != null)
+                                {
+                                    td.race.meatDef.description += nameAdd;
+                                    td.race.corpseDef.description += nameAdd;
+                                    if(td.race.leatherDef != null)
+                                        td.race.leatherDef.description += nameAdd;
+                                }
+
+                                /*if (td.recipeMaker != null)
+                                    DefDatabase<RecipeDef>.GetNamed(defName: "Make_" + td.defName).description += nameAdd;
+                                if(td.IsDrug)
+                                    DefDatabase<RecipeDef>.GetNamed(defName: "Administer_" + td.defName).description += nameAdd;*/
+                                break;
+                        }
+
+                        if (BackstoryDatabase.allBackstories.TryGetValue(key: def.defName, value: out Backstory bs))
+                            bs.baseDesc += nameAdd;
+
+
                     }
-
-                    if (BackstoryDatabase.allBackstories.TryGetValue(key: def.defName, value: out Backstory bs))
-                        bs.baseDesc += nameAdd;
+                    catch (Exception)
+                    {
+                        Log.Error(text: $"ModDesignator: {def.defName} of {def.GetType()} is evil");
+                        throw;
+                    }
                 }
         }
     }
